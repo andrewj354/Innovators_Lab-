1ee3eb9c3a378c468f07b1e02db77c234da3fb78
Rostyslav <roosolam@gmail.com>
2026-03-22 13:11:51 +0200

    auth views + urls + models

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -9,6 +9,15 @@ class CustomUser(AbstractUser):
         ('jury', 'Jury'),
     ]
     role = models.CharField(max_length=10, choices=ROLES, default='team')
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50,blank=True)
+    last_name = models.CharField(max_length=50,blank=True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
 
     def __str__(self):
         return f"{self.email} ({self.role})"
---
+++ backend/apps/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, LoginView, Verify2FAView,MeView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='user-register'),
+    path('login/', LoginView.as_view(), name='user-login'),
+    path('verify-2fa/', Verify2FAView.as_view(), name='verify-2fa'),
+    path('me/', MeView.as_view()),
+]
