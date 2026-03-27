808413a699d75e524f50ee8529d1b7f41d346279
Rostyslav <roosolam@gmail.com>
2026-03-27 20:03:38 +0200

    feat: додано модель Tournament

--- backend/apps/tournaments/models.py
+++ backend/apps/tournaments/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+
+class Tournament(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'Draft', 'Чорновик'
+        REGISTRATION = 'Registration', 'Реєстрація'
+        RUNNING = 'Running', 'Триває'
+        FINISHED = 'Finished', 'Завершено'
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    reg_start = models.DateTimeField()
+    reg_end = models.DateTimeField()
+    max_teams = models.PositiveIntegerField()
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='tournaments'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.status})"
