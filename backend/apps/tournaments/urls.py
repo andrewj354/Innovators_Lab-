9379b95f97fa8d2b3eaa88fb31ae52f1f50452c4
Rostyslav <roosolam@gmail.com>
2026-03-27 20:22:42 +0200

    feat: Додано статусну машину для турнірів

--- backend/apps/tournaments/urls.py
+++ backend/apps/tournaments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TournamentListCreateView, TournamentRetrieveUpdateDeleteView
+from .views import TournamentListCreateView, TournamentRetrieveUpdateDeleteView,TournamentStatusUpdateView
 
 urlpatterns = [
     path('', TournamentListCreateView.as_view(), name='tournament-list-create'),
     path('<int:pk>/', TournamentRetrieveUpdateDeleteView.as_view(), name='tournament-rud'),
+    path('<int:pk>/status/', TournamentStatusUpdateView.as_view(), name='tournament-status-update'),
 ]
