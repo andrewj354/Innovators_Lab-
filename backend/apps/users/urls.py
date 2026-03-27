1207bd146064e513839cedd8c33a237bf1d7d6a7
Rostyslav <roosolam@gmail.com>
2026-03-27 20:30:51 +0200

    debug

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import RegisterView, LoginView, Verify2FAView, MeView
+from .views import RegisterView, LoginView, Verify2FAView, MeView, GoogleLoginView , GoogleCallbackView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
