079caa1ac8d55cc7f194a575d83ff9d2d2c63632
Rostyslav <roosolam@gmail.com>
2026-03-27 20:06:20 +0200

    feat: Tournament serializers

---
+++ backend/apps/tournaments/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Tournament
+from apps.teams.models import Team  
+
+
+# Serializer для створення турніру (Admin)
+class TournamentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tournament
+        fields = [
+            'title', 'description', 'reg_start', 'reg_end',
+            'max_teams', 'status', 'created_by'
+        ]
+        read_only_fields = ['created_by']  
+
+
+
+# Публічний список турнірів
+class TournamentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tournament
+        fields = [
+            'id', 'title', 'description', 'reg_start',
+            'reg_end', 'status', 'max_teams'
+        ]
+
+
+# Деталі турніру + список команд
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = ['id', 'name', 'captain_email']  
+
+class TournamentDetailSerializer(serializers.ModelSerializer):
+    teams = TeamSerializer(many=True, read_only=True, source='team_set') 
+
+    class Meta:
+        model = Tournament
+        fields = [
+            'id', 'title', 'description', 'reg_start',
+            'reg_end', 'status', 'max_teams', 'teams'
+        ]
