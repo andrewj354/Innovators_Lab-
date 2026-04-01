225d226709057f90c607dc9e03f922c42d9c4f07
Rostyslav <roosolam@gmail.com>
2026-04-01 17:03:58 +0300

    init

--- backend/apps/jury/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JuryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.jury'
--- backend/apps/tournaments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TournamentListCreateView, TournamentRetrieveUpdateDeleteView,TournamentStatusUpdateView
-
-urlpatterns = [
-    path('', TournamentListCreateView.as_view(), name='tournament-list-create'),
-    path('<int:pk>/', TournamentRetrieveUpdateDeleteView.as_view(), name='tournament-rud'),
-    path('<int:pk>/status/', TournamentStatusUpdateView.as_view(), name='tournament-status-update'),
-]
